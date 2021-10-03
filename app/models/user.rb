5018e0edc431d3b6882a9bd8b1fc364a228c70bd
Heidiki <82322691+Heidiki@users.noreply.github.com>
2021-10-03 18:19:52 +0900

    ユーザ登録・ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,22 @@ class User < ApplicationRecord
   belongs_to :gender
   belongs_to :nationality
   belongs_to :occupation
+
+  with_options presence: true do
+    validates :nickname
+    validates :gender_id
+    validates :user_age, format: {with: /\A[0-9]+\z/, message: "is invalid. Input only number"}
+    validates :nationality_id
+    validates :occupation_id
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :gender_id
+    validates :nationality_id
+    validates :occupation_id
+  end
+
+  validates :password,
+            format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: 'is invalid. Include both letters and numbers' }
+
 end
