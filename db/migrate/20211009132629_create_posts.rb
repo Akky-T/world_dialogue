ba7a1e859caf469a0aaf6e24a25cb23594e78979
Heidiki <82322691+Heidiki@users.noreply.github.com>
2021-10-10 23:09:54 +0900

    post・ActiveStorageテーブルの実装

--- db/migrate/20211009132629_create_posts.rb
+++ db/migrate/20211009132629_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.string    :title,          null: false
+      t.text      :post_text,      null: false
+      t.references :user,           null: false
+      t.integer   :to_national_id, null: false
       t.timestamps
     end
   end
