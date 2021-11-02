5fa22375890f653952712e65b7b14e112847a794
Heidiki <82322691+Heidiki@users.noreply.github.com>
2021-11-02 22:12:20 +0900

    JavaScriptのEurope導入

--- app/javascript/maps.js
+++ app/javascript/maps.js
@@ -1,5 +1,5 @@
 function post (){
-  const link_azia_oceania = document.getElementById("link_europe");
+  const link_azia_oceania = document.getElementById("europe_map");
   link_azia_oceania.addEventListener("click", () => {
     console.log("イベント発火");
   });
